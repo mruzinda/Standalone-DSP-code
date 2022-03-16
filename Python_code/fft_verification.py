32b6eb1b527e311de4f477a3beee40e56d5dc337
Mark Ruzindana <mruzinda@blc03.bl.pvt>
2022-03-16 23:38:42 +0000

    Added FFT verification script called fft_verification.py that still needs to be tested. And modified plot_cufft_output_numpy.py to correctly plot the output from the CUDA upchannelizer code.

--- Python_code/fft_verification.py
+++ Python_code/fft_verification.py
@@ -1,19 +1,21 @@
-# This script reshapes the one dimensional array from the beamformer and plots the response for analysis
+# Verify whether the same results are seen from the cuFFT in the upchannelizer code
+
 import matplotlib.pyplot as plt
 from array import array
 import numpy as np
 
+from numpy.fft import fft, ifft
+
 # Open text file containing beamformer output
-filename = "/datag/users/mruzinda/o/output_d_cufft.bin"
+filename = "/datag/users/mruzinda/i/input_h_cufft.bin"
 
 # Read file contents: np.fromfile(filename, dtype=float, count=- 1, sep='', offset=0)
 contents_float = np.fromfile(filename, dtype=np.float32)
 
-# Read file contents
-#contents = bytearray(f.read(4*2*131072))
-
 print(len(contents_float))
 print(contents_float[0])
+
+# data_in_idx(p, t, w, c, a, Np, Nt, Nw, Nc)
 
 # Array dimensions
 # 1k mode
@@ -23,26 +25,27 @@ print(contents_float[0])
 #N_fine = (1024*1024)/8 # 2^17 
 #N_coarse = 4 # 4
 # 32k mode
-N_fine = (128*1024)/8 # 2^14
+N_time = (128*1024)/8 # 2^14
+N_fine = N_time
 N_coarse = 32 # 4
 
-N_win = 8
 N_pol = 2
+N_win = 8
 N_ant = 64 
 N_iq = 2
 
 # Reshape array to 3D -> Antenna X Polarization X Time X Bins
-contents_array = contents_float[0:(N_coarse*N_win*N_pol*N_ant*N_fine*N_iq)].reshape(N_win,N_coarse,N_pol,N_ant,N_fine,N_iq)
+contents_array = contents_float[0:(N_coarse*N_win*N_pol*N_ant*N_time*N_iq)].reshape(N_ant,N_coarse,N_win,N_time,N_pol,N_iq)
 
-contents_restructure = np.zeros(N_win*N_pol*N_ant*N_coarse*N_fine*N_iq).reshape(N_win,N_pol,N_ant,N_coarse*N_fine,N_iq)
-
+x = np.zeros(N_win*N_pol*N_ant*N_coarse*N_time*N_iq).reshape(N_win,N_pol,N_ant,N_coarse,N_time,N_iq)
+X = np.zeros(N_win*N_pol*N_ant*N_coarse*N_fine*N_iq).reshape(N_win,N_pol,N_ant,N_coarse*N_fine,N_iq)
 # Combine coarse and fine channels
 for c in range(0,N_coarse):
     for w in range(0,N_win):
         for p in range(0,N_pol):
             for a in range(0,N_ant):
                 for iq in range(0,N_iq):
-                    contents_restructure[w,p,a,(0+c*N_fine):(N_fine+c*N_fine),iq] = contents_array[w,c,p,a,0:N_fine,iq]
+                    X[w,p,a,(0+c*N_fine):(N_fine+c*N_fine),iq] = fft(contents_array[a,c,w,0:N_time,p,iq])
 
 ant_idx = 0 # beam index to plot
 pol_idx = 0 # polarization index to plot
@@ -54,7 +57,7 @@ iq_idx = 0 # Real or imaginary component
 # I'm only removing it for the sake of accurate analysis and diagnosis.
 #plt.imshow(contents_array[0:N_win,0:N_coarse,beam_idx], extent=[1, N_coarse, 1, N_win], aspect='auto', interpolation='bicubic')
 #plt.imshow(contents_array[coarse_idx,0:N_win,pol_idx,ant_idx,0:N_fine,iq_idx], extent=[1, N_fine, 1, N_win], aspect='auto', interpolation='none')
-plt.imshow(contents_restructure[0:N_win,pol_idx,ant_idx,0:N_coarse*N_fine,iq_idx], extent=[1, N_coarse*N_fine, 1, N_win], aspect='auto', interpolation='none')
+plt.imshow(X[0:N_win,pol_idx,ant_idx,0:N_coarse*N_fine,iq_idx], extent=[1, N_coarse*N_fine, 1, N_win], aspect='auto', interpolation='none')
 plt.title('Intensity map (Frequency vs. time)')
 plt.ylabel('Time samples')
 plt.xlabel('Frequency bins')
@@ -62,7 +65,7 @@ plt.show()
 
 # Plot of power spectrum
 #plt.plot(contents_array[coarse_idx,time_idx,pol_idx,ant_idx,0:N_fine,iq_idx])
-plt.plot(contents_restructure[time_idx,pol_idx,ant_idx,0:N_coarse*N_fine,iq_idx])
+plt.plot(X[time_idx,pol_idx,ant_idx,0:N_coarse*N_fine,iq_idx])
 plt.title('FFT at a time window')
 plt.xlabel('Fine frequency channels')
 plt.ylabel('Raw voltage (arb.)')
@@ -70,13 +73,13 @@ plt.show()
 
 fig, axs = plt.subplots(2, 2)
 fig.suptitle('FFT at different antennas')
-axs[0, 0].plot(contents_restructure[time_idx,pol_idx,0,0:N_coarse*N_fine,iq_idx])
+axs[0, 0].plot(X[time_idx,pol_idx,0,0:N_coarse*N_fine,iq_idx])
 axs[0, 0].set_title('Ant 1')
-axs[0, 1].plot(contents_restructure[time_idx,pol_idx,1,0:N_coarse*N_fine,iq_idx], 'tab:orange')
+axs[0, 1].plot(X[time_idx,pol_idx,1,0:N_coarse*N_fine,iq_idx], 'tab:orange')
 axs[0, 1].set_title('Ant 2')
-axs[1, 0].plot(contents_restructure[time_idx,pol_idx,2,0:N_coarse*N_fine,iq_idx], 'tab:green')
+axs[1, 0].plot(X[time_idx,pol_idx,2,0:N_coarse*N_fine,iq_idx], 'tab:green')
 axs[1, 0].set_title('Ant 3')
-axs[1, 1].plot(contents_restructure[time_idx,pol_idx,57,0:N_coarse*N_fine,iq_idx], 'tab:red')
+axs[1, 1].plot(X[time_idx,pol_idx,57,0:N_coarse*N_fine,iq_idx], 'tab:red')
 axs[1, 1].set_title('Ant 57')
 
 # set the spacing between subplots
@@ -94,4 +97,3 @@ for ax in axs.flat:
 #for ax in axs.flat:
 #    ax.label_outer()
 plt.show()
-
